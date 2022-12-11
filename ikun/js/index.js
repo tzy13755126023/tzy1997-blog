c0080dc0a5c9e5eb6ec0671de7d1e5b9adbd4d1f
tzy <2938526863@qq.com>
2022-12-11 20:51:38 +0800

    update ikun index.js

--- ikun/js/index.js
+++ ikun/js/index.js
@@ -1,17 +1,24 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: tzy1997
+ * @Date: 2022-12-05 18:09:55
+ * @LastEditors: tzy1997
+ * @LastEditTime: 2022-12-11 20:49:20
+ */
 var audio = document.getElementById('audio');
 audio.play();
 var lis = document.querySelectorAll('ul.jj li');
 lis.forEach(li => {
     li.onclick = function() {
 		if(this.innerHTML=='黑人抬鸡(棺)'){
-			audio.src = './audio/' + this.getAttribute('m') + '.mpeg';
+			audio.src = 'https://npm.elemecdn.com/ethan4116-blog/ikun/audio/' + this.getAttribute('m') + '.mpeg';
 			audio.play();
 		}else{
-			audio.src = './audio/' + this.getAttribute('m') + '.mp3';
+			audio.src = 'https://npm.elemecdn.com/ethan4116-blog/ikun/audio/' + this.getAttribute('m') + '.mp3';
 			audio.play();
 			console.log(this.innerHTML)
 		}
-        
     }
 });
 document.querySelector('.s1').onclick = function() {
